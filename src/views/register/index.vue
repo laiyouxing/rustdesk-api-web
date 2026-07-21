956ee1e57c246f44250d22a45a8b7f71ed8f9d38
laiyouxing <151647251+laiyouxing@users.noreply.github.com>
2026-07-21 23:20:11 +0800

    注册页: 邀请码提示改为'请联系管理员获取', 下方显示管理员邮箱

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -26,6 +26,9 @@
               <el-tag type="success">{{ T('Active') }}</el-tag>
             </template>
           </el-input>
+          <div style="margin-top:4px; font-size:12px; color:#909399;">
+            管理员：laiyouxing@foxmail.com
+          </div>
         </el-form-item>
         <el-form-item label="">
           <el-button @click="submit" class="login-button" type="success">{{ T('Submit') }}</el-button>
